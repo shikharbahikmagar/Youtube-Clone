8f2362436bb11e164731d99215a55908be173dec
shikharbahikmagar <shikharbahik5@gmail.com>
2024-08-18 13:56:06 +0545

    Update Sidebar component with larger icons and responsive layout

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -18,20 +18,20 @@ function Sidebar (){
             <ul>
                 <li>
                     <a href="#" className="flex items-center p-3 hover:rounded-lg hover:bg-gray-300">
-                        <IoMdHome className="text-sm"/>
+                        <IoMdHome className="text-lg"/>
 
                         <span className="ml-3 text-sm">Home</span>
                     </a>
                 </li>
                 <li>
                     <a href="#" className="flex items-center p-3 hover:rounded-lg hover:bg-gray-300">
-                        <SiYoutubeshorts className="text-sm"/>
+                        <SiYoutubeshorts className="text-lg"/>
                         <span className="ml-3 text-sm">Shorts</span>
                     </a>
                 </li>
                 <li>
                     <a href="#" className="flex items-center p-3 hover:rounded-lg hover:bg-gray-300">
-                    <MdSubscriptions className="text-sm"/>
+                    <MdSubscriptions className="text-lg"/>
                         <span className="ml-3 text-sm">Subscriptions</span>
                     </a>
                 </li>
@@ -41,7 +41,32 @@ function Sidebar (){
 
     )
     else
-    return null;
+    return (
+    <aside className="w-24 bg-gray-100 text-slate-700 text:sm min-h-screen flex flex-col">
+    <nav className="flex-1 items-center align-center mt-4">
+        <ul>
+            <li>
+                <a href="#" className="flex flex-col align-center items-center p-3 hover:rounded-lg hover:bg-gray-300">
+                    <IoMdHome className="text-xl"/>
+                    <span className="mt-1 text-[12px]">Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" className="flex flex-col align-center items-center p-3 hover:rounded-lg hover:bg-gray-300">
+                    <SiYoutubeshorts className="text-xl"/>
+                    <span className="mt-1 text-[12px]">Shorts</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" className="flex flex-col align-center items-center p-3 hover:rounded-lg hover:bg-gray-300">
+                <MdSubscriptions className="text-xl"/>
+                    <span className="mt-1 text-[12px]">Subscriptions</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
+)
 } 
 
 export default Sidebar
