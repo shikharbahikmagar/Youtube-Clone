271072e777d16561c86feb73e4073bd67d48eaf0
shikharbahikmagar <shikharbahik5@gmail.com>
2024-09-10 10:45:09 +0545

    feat: Add support for additional HTTP methods in CORS configuration

--- js_backend/src/app.js
+++ js_backend/src/app.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
 }))
 
 app.use(express.json({
