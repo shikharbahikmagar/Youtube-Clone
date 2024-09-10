533675cdbfed794cd2a119c562eefeddd3fe9022
shikharbahikmagar <shikharbahik5@gmail.com>
2024-09-10 10:39:53 +0545

    refactor: Update backend base URL to use environment variable

--- js_backend/src/index.js
+++ js_backend/src/index.js
@@ -11,9 +11,9 @@ dotenv.config({
 connectDB()
 .then(() => {
 
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 3000, () => {
 
-        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+        console.log(`Server is running on port ${process.env.PORT || 3000}`)
     })
 
     app.on("error", (error) => {
