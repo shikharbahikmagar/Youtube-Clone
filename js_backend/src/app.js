10ea56f414bed6f4d590e40c72f07fd5d9b21b18
Shikhar bahik magar <38580729+shikharbahikmagar@users.noreply.github.com>
2024-11-21 01:06:09 -0800

    Update app.js

--- js_backend/src/app.js
+++ js_backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://youtube-clone-ten-coral.vercel.app/',
     credentials: true,
     methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
 }))
